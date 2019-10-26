1865654e574559f2b90911c7cf6e856d7b3d7de7
Fady El-Gendy <fadyelgendy2017@gmail.com>
2019-10-26 10:39:40 +0200

    initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        //scaffold the tables class to database model
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        // create values table
+        public DbSet<Value> Values { get; set; } // represnt the values table in sql db
+    }
+
+}
